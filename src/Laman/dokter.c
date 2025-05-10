79c8b4383dd079d54b718cb08e8d5151c622d0ca
ashkar <13524007@std.stei.itb.ac.id>
2025-05-10 21:34:58 +0700

    lamanDokter

--- src/Laman/dokter.c
+++ src/Laman/dokter.c
@@ -1,5 +1,64 @@
 #include "dokter.h"
 
+void lamanDokter(){
+    extern User *user;
+    extern PilihanDokter pilihanD;
+    int input;
+    int valid = 0;
+
+    while (!valid) {
+        clearScreen();
+        labelUser();
+        printf("\nSelamat pagi %s! Ada keluhan apa?\n\n",user->identitas.username);
+        labelMenuPasien();
+        printf(">>> Masukkan pilihan (1-3): ");
+        
+        int ret = scanf("%d", &input);
+
+        if(ret != 1){
+            while(getchar() != '\n'); // flush input buffer
+            printf("\nInput tidak valid! Masukkan angka antara 1-3.");
+            printf("\nSilahkan enter untuk mengulang!\n");
+            getchar(); // tunggu enter
+            continue;
+        }
+        else if (input != 1 && input != 2 && input != 3){
+            // input bukan angka
+            while(getchar() != '\n'); // flush input buffer
+            printf("\nInput tidak valid! Masukkan angka antara 1-3.");
+            printf("\nSilahkan enter untuk mengulang!\n");
+            getchar(); // tunggu enter
+            continue;
+        }
+
+        switch (input) {
+            case 1: case 2:
+                pilihanD = input;
+                valid = 1;
+                break;
+            case 3:
+                char c;
+                do{
+                    printf("Yakin mau logout? (y/n)\n");
+                    scanf(" %c",&c);
+                    if(c=='n' || c=='N'){
+                        lamanPasien();
+                    }
+                }while(c != 'y' && c != 'n' && c != 'Y' && c != 'N');
+                pilihanD = LOGOUTP;
+                valid = 1;
+                break;
+            default:
+                printf("\nPilihan tidak valid! Silakan ulangi.\n");
+                getchar(); // tunggu enter
+        }
+    }
+
+    char *opsi[] = {"", "DIAGNOSIS", "NGOBATIN"};
+    if (pilihanD != LOGOUTD)
+        printf("\n>>> %s\n\n", opsi[pilihanD]);
+}
+
 void diagnosis(){
     extern Penyakit *penyakits;
     extern int jumlah_penyakit;
@@ -25,7 +84,9 @@ void diagnosis(){
         if (pasien->kondisi.tinggi_badan > penyakits[i].tinggi_badan_min && pasien->kondisi.tinggi_badan < penyakits[i].tinggi_badan_max) continue;
         if (pasien->kondisi.kadar_kolesterol > penyakits[i].kadar_kolesterol_min && pasien->kondisi.kadar_kolesterol < penyakits[i].kadar_kolesterol_max) continue;
         if (pasien->kondisi.trombosit > penyakits[i].trombosit_min && pasien->kondisi.trombosit < penyakits[i].trombosit_max) continue;
+
         id_penyakit = penyakits[i].id;
+        strcpy(pasien->identitas.riwayat_penyakit, penyakits[i].nama);
         printf("%s terdiagnosa penyakit %s!\n", pasien->identitas.username);
         return;
     }
