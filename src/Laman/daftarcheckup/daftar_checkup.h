b5d3ddfd61a49147e0f17174944a86f9d2331a6b
aureliajenn <aurellia.jennifer@gmail.com>
2025-05-21 14:35:15 +0700

    feat : implementasi fitur daftar checkup

---
+++ src/Laman/daftarcheckup/daftar_checkup.h
@@ -0,0 +1,21 @@
+#ifndef CHECKUP_H
+#define CHECKUP_H
+
+#define MAX_NAME 50
+
+#include "queue.h"
+
+typedef struct {
+    char nama_dokter[MAX_NAME];
+    char ruangan[MAX_NAME];
+    Queue queue;
+} Dokter;
+
+typedef struct {
+    char nama_pasien[MAX_NAME];
+    char nama_dokter[MAX_NAME];
+} MapPasien;
+
+void daftar_checkup(const char* pasien, Dokter* dokter_list, int jumlah_dokter, MapPasien* map, int* jumlah_map);
+
+#endif
