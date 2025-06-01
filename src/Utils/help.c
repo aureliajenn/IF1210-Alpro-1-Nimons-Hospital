81e3de8487834654ac6d600cb7f7c25039f15d21
aureliajenn <aurellia.jennifer@gmail.com>
2025-06-02 02:54:11 +0700

    feat: penambahan comman lupa password dan denah pada menu and help

--- src/Utils/help.c
+++ src/Utils/help.c
@@ -9,23 +9,32 @@ void help(const char role[], const char username[]) {
     } else if (strcmp(role, "Dokter") == 0) {
         printf("Halo Dokter %s. Kamu memanggil command HELP. Kamu pasti sedang kebingungan. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:\n\n", username);
         printf("1. LOGOUT: Keluar dari akun yang sedang digunakan\n");
-        printf("2. DIAGNOSIS: Melakukan diagnosis penyakit pasien berdasarkan kondisi tubuh pasien\n");
-        printf("3. NGOBATIN: Memberi obat kepada pasien secara terurut\n\n");
+        printf("2. LUPA_PASSWORD: Mengubah password");
+        printf("3. DIAGNOSIS: Melakukan diagnosis penyakit pasien berdasarkan kondisi tubuh pasien\n");
+        printf("4. NGOBATIN: Memberi obat kepada pasien secara terurut\n\n");
+        printf("5. LIHAT_DENAH: Menampilkan layout denah ruamh sakit");
+        printf("6. LIHAT_RUANGAN: Menampilkan detail ruangan");
     } else if (strcmp(role, "Pasien") == 0) {
         printf("Selamat datang, %s. Kamu memanggil command HELP. Kamu pasti sedang kebingungan. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:\n\n", username);
         printf("1. LOGOUT: Keluar dari akun yang sedang digunakan\n");
-        printf("2. DAFTAR_CHECKUP: Mendaftarkan diri untuk pemeriksaan dokter\n");
-        printf("3. AKU_BOLEH_PULANG_GA_DOK: Melakukan pemeriksaan ulang kepada dokter untuk memastikan apakah pasien benar-benar sudah siap untuk pulang\n");
-        printf("4. ANTRIAN_SAYA: Melihat status antrian\n");
-        printf("5. MINUM_OBAT: Meminum obat yang telah diberikan oleh dokter\n");
+        printf("2. LUPA_PASSWORD: Mengubah password");
+        printf("3. DAFTAR_CHECKUP: Mendaftarkan diri untuk pemeriksaan dokter\n");
+        printf("4. AKU_BOLEH_PULANG_GA_DOK: Melakukan pemeriksaan ulang kepada dokter untuk memastikan apakah pasien benar-benar sudah siap untuk pulang\n");
+        printf("5. ANTRIAN_SAYA: Melihat status antrian\n");
+        printf("6. MINUM_OBAT: Meminum obat yang telah diberikan oleh dokter\n");
         printf("6. MINUM_PENAWAR: Mengeluarkan obat terakhir yang diminum dan mengembalikannya ke dalam inventory obat\n\n");
+        printf("7. LIHAT_DENAH: Menampilkan layout denah ruamh sakit");
+        printf("8. LIHAT_RUANGAN: Menampilkan detail ruangan");
     } else if (strcmp(role, "Manager") == 0) {
         printf("Halo Manager %s. Kenapa kamu memanggil command HELP? Kan kamu manager, tapi yasudahlah kamu pasti sedang kebingungan. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:\n\n", username);
         printf("1. LOGOUT: Keluar dari akun yang sedang digunakan\n");
-        printf("2. TAMBAH_DOKTER: Mendaftarkan dokter baru ke sistem\n");
-        printf("3. LIHAT_USER: Melihat data seluruh pengguna (dokter dan pasien), atau secara spesifik hanya pasien atau hanya dokter\n");
-        printf("4. CARI_USER: Mencari data pengguna secara spesifik berdasarkan ID atau Nama melalui perintah (CARI_USER), maupun secara lebih terfokus melalui (CARI_PASIEN) dan (CARI_DOKTER)\n");
-        printf("5. LIHAT_ANTRIAN: Melihat rincian di seluruh ruangan saat ini. Ruangan yang perlu ditampilkan adalah ruangan yang tidak kosong\n\n");
+        printf("2. LUPA_PASSWORD: Mengubah password");
+        printf("3. TAMBAH_DOKTER: Mendaftarkan dokter baru ke sistem\n");
+        printf("4. LIHAT_USER: Melihat data seluruh pengguna (dokter dan pasien), atau secara spesifik hanya pasien atau hanya dokter\n");
+        printf("5. CARI_USER: Mencari data pengguna secara spesifik berdasarkan ID atau Nama melalui perintah (CARI_USER), maupun secara lebih terfokus melalui (CARI_PASIEN) dan (CARI_DOKTER)\n");
+        printf("6. LIHAT_ANTRIAN: Melihat rincian di seluruh ruangan saat ini. Ruangan yang perlu ditampilkan adalah ruangan yang tidak kosong\n\n");
+        printf("7. LIHAT_DENAH: Menampilkan layout denah ruamh sakit");
+        printf("8. LIHAT_RUANGAN: Menampilkan detail ruangan");
     }
 
     printf("Footnote:\n");
